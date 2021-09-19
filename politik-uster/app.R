693ddaf41c30900f166cafa5497b6f71a0e4d8fe
socialthink <78662247+socialthink@users.noreply.github.com>
2021-09-19 23:19:19 +0200

    update

--- politik-uster/app.R
+++ politik-uster/app.R
@@ -4,6 +4,11 @@ library(igraph)
 library(fmsb)
 library(dplyr)
 
+library(shinythemes)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+
 
 ## Daten-Import ======================
 
@@ -35,19 +40,14 @@ gr_auswahl <- list(`aktive`=gr_auswahl_aktiv,
 
 
 # UI ============
-ui <- fluidPage(
+ui <- 
     
     # Application title
-    titlePanel("Politikanalysen für Uster ZH"),
+    navbarPage("Politikanalysen für Uster ZH",theme = shinytheme("journal"),
     
     # Sidebar with a slider input for number of bins 
-    fluidRow(
-        
-        
-        # Show a plot of the generated distribution
-        column(9,
+
                
-               tabsetPanel(type = "tabs",
                            
                            ### TAB: Übersicht ====   
                            #tabPanel("Übersicht",
@@ -66,21 +66,24 @@ ui <- fluidPage(
                            ### TAB: Die einzelnen Gemeinderät:innen ====   
                            tabPanel("Gemeinderät:innen",
                                     fluidRow(
-                                        column(4,HTML("<br>"),wellPanel(
+                                        column(3,HTML("<br>"),wellPanel(
                                             selectInput("politikfeld", h4("Gemeinderät:in auswählen"), 
                                                         choices = gr_auswahl),
                                             HTML("Du kannst den Namen im Feld löschen und mit der Tastatur nach Gemeinderät:innen suchen.")
                                         )),
-                                        column(6,
+                                        column(9,
                                                htmlOutput("gr_auswahl",container=tags$h2),
-                                               plotOutput("plot_politikfeld")
+                                               fluidRow(
+                                                   column(6,plotOutput("plot_politikfeld")),
+                                                   column(6,plotOutput("plot_wordcloud"))
+                                               )
                                         )
                                     ),
                                     fluidRow(
-                                        column(4,
+                                        column(3,
                                                HTML("<br>")
                                         ),
-                                        column(8,
+                                        column(9,
                                                HTML("<br>"),
                                                dataTableOutput('gr_gesch_table')
                                                
@@ -90,37 +93,43 @@ ui <- fluidPage(
                            ### TAB: Netzwerkanalye ====   
                            tabPanel("Netzwerkanalyse",
                                     fluidRow(
-                                        column(4,HTML("<br>"),wellPanel(
-                                            #radioButtons("radio_sna_gr", h4("Gemeinderät:innen"),
-                                            #             choices = list("nur aktuelle Gemeinderät:innen" = 1, "alle Gemeinderät:innen" = 2),selected = 1),
-                                            #radioButtons("radio_sna_legislatur", h4("Legislatur"),
-                                            #             choices = list("ALLE" = 1, "2018-2022" = 2,
-                                            #                            "2014-2018" = 3, "2010-2014" = 4),selected = 1),
+                                        column(3,HTML("<br>"),wellPanel(
                                             
-                                            dateRangeInput('dateRange',
-                                                           label = 'Welcher Zeitraum soll berücksichtigt werden?',
-                                                           start = "2018-05-01" , end = "2022-05-01"
-                                            ),
                                             radioButtons("radio_sna_cluster", "Welche Methode zur Cluster-Bildung soll verwendet werden?",
                                                          choices = list("Louvain Community Detection" = 1, "Infomap" = 2,
                                                                         "Edge betweenness" = 3),selected = 1),
-                                            HTML("<br><b>Erklärung Analysemethode:</b><br>Es wird jeweils analysiert, welche Personen gemeinsam parlamentarische Instrumente nutzten (z.B. eine Anfrage einreichen). Aus dieser Information kann ein gemeinsames poltisches Interesse und Handeln abgeleitet werden, welches zur Netzwerkanalyse und Clusterbildung genutzt wird. Personen die ihre parlamentatischen Instrumente mit niemand anderem nutzen, werden aus der Analyse ausgeschlossen.")
+                                            HTML("<br>"),
+                                            radioButtons("radio_sna_legislatur", "Zeitraum",
+                                                         choices = list("Legislatur 2018-2022" = 1,
+                                                                        "Legislatur 2014-2018" = 2,
+                                                                        "Legislatur 2010-2014" = 3,
+                                                                        "sonstiger Zeitraum:" = 4),selected = 1),
+                                            
+                                            dateRangeInput('dateRange',
+                                                           label = NULL,
+                                                           start = "2021-05-01" , end = "2022-05-01", format = "dd.mm.yyyy", language = "de", separator = "bis", weekstart = 1
+                                            ),
+                                            HTML("<br><b>Erklärung Analysemethode:</b><br>Es wird jeweils analysiert, welche Personen gemeinsam parlamentarische Instrumente einsetzen (z.B. eine Anfrage einreichen). Aus dieser Information kann ein gemeinsames politisches Interesse und Handeln abgeleitet werden, welches zur Netzwerkanalyse und Clusterbildung genutzt wird. Personen die ihre parlamentarischen Instrumente nur alleine nutzen, werden aus der Analyse ausgeschlossen.")
                                         )),
-                                        column(8,
-                                               plotOutput("plot_sna")
+                                        column(9,
+                                               plotOutput("plot_sna", width = "100%", height = "700px",)
                                         )
+                                    )
+                           ),
+                           ### TAB: About ====   
+                           tabPanel("About",
+                                    fluidRow(
+                                        column(9,HTML("<br>"),wellPanel(
+                                               HTML("<b>About</b><br>Dieses kommunale Politikbarometer ist ein privates Projekt von Andreas Wyss, es handelt sich um technische und statistische Spielereien anhand der auf der Webseite uster.ch verfügbaren Daten zu den Gemeinderatsgeschäften.
+                    <br><br><b>Lizenzen</b><br>Die Daten stehen unter Public Domain (Verzeichnis Gemeinderatsgeschäfte) resp. CC BY 4.0 (Stichwortliste und Verzeichnis Gemeinderät:innen). Der Code für die Auswertung wird unter GPLv3 zur Verfügung gestellt. Details sowie die Datensätze und Quellcode findest du unter <a href=\"https://github.com/data-socialthink/politik-uster\" target=_blank>github.com/data-socialthink/politik-uster</a>.")
+                                        ))
+
                                     )
                            )
                            
                )
-               
-        ),
-        column(3,
-               HTML("<b>About</b><br>Dieses kommunale Politikbarometer ist ein privates Projekt von Andreas Wyss, es handelt sich um technische und statistische Spielereien anhand der auf der Webseite uster.ch verfügbaren Daten zu den Gemeinderatsgeschäften.
-                    <br><br><b>Lizenzen</b><br>Die Daten stehen unter Public Domain (Verzeichnis Gemeinderatsgeschäfte) resp. CC BY 4.0 (Stichwortliste und Verzeichnis Gemeinderät:innen). Die Software wird unter einer MIT-Lizenz zur Verfügung gestellt. Details sowie die Datensätze und Quellcode findest du unter <a href=\"https://github.com/data-socialthink/politik-uster\" target=_blank>github.com/data-socialthink/politik-uster</a>.")
-        )
-    )
-)
+
+
 
 # SERVER ==========
 server <- function(input, output) {
@@ -181,6 +190,54 @@ server <- function(input, output) {
         radarchart(rbind(max(dfplot),0,dfplot),pfcol="red")
     })
     
+    output$plot_wordcloud <- renderCachedPlot({
+        gr_nummer_wordcloud <- as.numeric(input$politikfeld)
+        gr_geschaft_wordcloud <- as.data.table(Geschafte_alle)[V1 %like% paste0(GR_Liste$Vorname," ",GR_Liste$Name)[gr_nummer_wordcloud]]
+        
+        filterbegriffe <- paste0(GR_Liste$Vorname," ",GR_Liste$Name)
+        filterbegriffe <- c(filterbegriffe,GR_Liste[,4])
+        filterbegriffe <- c(filterbegriffe,"antrag","betreffend","stadtrat","stadtrates","uster","anfrage","ratsmitglied","antwort","postulat","genehmigung","primarschulpflege","leistungsmotion","sekundarschulpflege","werner","weisung","motion","interpellation","ratsmitglieder","ratsmitgliedes","stadt","kessler","grüne","verordnung","geschäftsbericht","grünliberale","bdp","jfu","evp","svp","fdp","sp","grüne","partei")
+        
+        #Textanalyse
+        doc <- VCorpus(VectorSource(gr_geschaft_wordcloud))
+        toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+        doc <- tm_map(doc, toSpace, "/")
+        doc <- tm_map(doc, toSpace, "\\|")
+        
+        doc <- tm_map(doc, removeWords, filterbegriffe)
+        doc <- tm_map(doc, content_transformer(tolower))
+        doc <- tm_map(doc, removeNumbers)
+        doc <- tm_map(doc, removeWords, stopwords("german"))
+        doc <- tm_map(doc, removeWords, filterbegriffe) 
+        doc <- tm_map(doc, toSpace, "&amp")
+        doc <- tm_map(doc, removePunctuation)
+        doc <- tm_map(doc, toSpace, ":")
+        doc <- tm_map(doc, toSpace, ",")
+        doc <- tm_map(doc, stripWhitespace)
+        #doc <- tm_map(doc, stemDocument)
+        tdm <- TermDocumentMatrix(doc)
+        m <- as.matrix(tdm)
+        v <- sort(rowSums(m),decreasing=TRUE)
+        df <- data.frame(word = names(v),freq=v)
+        
+        #Wörterwolke
+        
+        if (dim(df)[1]>0) {
+            set.seed(1001)
+            par(mar=c(0,0,0,0))
+            wordcloud(words = df$word, freq = df$freq, min.freq = 1, scale=c(2,0.5),
+                      max.words=80, random.order=FALSE, rot.per=0.35, 
+                      colors=brewer.pal(8, "Dark2"))
+        } else {
+            set.seed(1001)
+            par(mar=c(0,0,0,0))
+            wordcloud(words = c("bisher","keine","Vorstösse"), freq = c(1,1,1), min.freq = 1, scale=c(4,0.4),
+                      max.words=80, random.order=FALSE, rot.per=0.35, 
+                      colors=brewer.pal(8, "Dark2"))
+        }
+        
+    }, cacheKeyExpr = { list(input$politikfeld)}, sizePolicy = sizeGrowthRatio(width = 800, height = 800, growthRate = 1), res = 180,)
+    
     output$gr_gesch_table <- renderDataTable({
         gr_nummer <- as.numeric(input$politikfeld)
         Geschafte_Liste_GR_anzeige <- arrange(Geschafte_Liste, desc(Date))
@@ -193,6 +250,8 @@ server <- function(input, output) {
                                    bFilter=1,                                    # global search box on/off
                                    bInfo=0# information on/off (how many records filtered, etc)
     ))
+    
+    
     
     ## SNA ==============
     
@@ -209,15 +268,29 @@ server <- function(input, output) {
     GR_Liste$bis <- as.Date(GR_Liste$bis)
     
     
-    
-    output$plot_sna <- renderPlot({
-        
+    output$plot_sna <- renderCachedPlot({
+
         #GR_Liste_aktiv <- GR_Liste %>% dplyr::filter(aktiv=="aktiv")
         #GR_Liste_aktiv <- GR_Liste %>% dplyr::filter(bis>="2018-05-2018") %>% dplyr::filter(von<="2022-05-07")
         GR_Liste_aktiv <- GR_Liste
         
         Geschafte_Liste$Date <- as.Date(Geschafte_Liste$Date)
-        Geschafte_Liste_filter <- Geschafte_Liste %>% dplyr::filter(Date >= input$dateRange[1]) %>% dplyr::filter(Date <= input$dateRange[2])
+        
+        if (input$radio_sna_legislatur == 1) {
+            datum_von <- as.Date("2018-05-07")
+            datum_bis <- as.Date("2022-05-08")
+        } else if (input$radio_sna_legislatur == 2) {
+            datum_von <- as.Date("2014-04-14")
+            datum_bis <- as.Date("2018-05-06")
+        } else if (input$radio_sna_legislatur == 3){
+            datum_von <- as.Date("2010-05-10")
+            datum_bis <- as.Date("2014-04-13")
+        } else if (input$radio_sna_legislatur == 4){
+            datum_von <- input$dateRange[1]
+            datum_bis <- input$dateRange[2]
+        }
+        
+        Geschafte_Liste_filter <- Geschafte_Liste %>% dplyr::filter(Date >= datum_von) %>% dplyr::filter(Date <= datum_bis)
         
         Geschafte_alle <- data.table(Geschafte_Liste_filter[,4])
         
@@ -242,7 +315,10 @@ server <- function(input, output) {
         # igraph  ======
         network <- graph_from_adjacency_matrix(netzwerk_matrix , mode='upper', weighted = T, diag=F)
         
+        
         # Plot
+        
+        
         
         if (input$radio_sna_cluster == 1)  {
             network_plot <- delete.isolates(network)
@@ -263,7 +339,7 @@ server <- function(input, output) {
             par(mar=c(0,0,0,0))
             print(plot(network_cluster, network_plot, vertex.label.cex=0.6))}
         
-    }, width = "auto", height = 700)
+    }, cacheKeyExpr = { list(input$radio_sna_cluster, input$dateRange, input$radio_sna_legislatur, GR_Liste, Geschafte_Liste)}, sizePolicy = sizeGrowthRatio(width = 1600, height = 1400, growthRate = 1), res = 180,)
     
 }
 
